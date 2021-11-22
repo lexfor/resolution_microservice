b01aaffe2f11736dd21f30c8613e15115ae960e8
Tsimafei Kinevich <tsimafei.kinevich@itrexgroup.lan>
2021-11-22 11:56:01 +0300

    fixes param store

--- src/domain/resolution/resolution.controller.ts
+++ src/domain/resolution/resolution.controller.ts
@@ -46,7 +46,7 @@ export class ResolutionController implements OnModuleInit {
   @Client({
     transport: Transport.GRPC,
     options: {
-      url: 'ec2-18-217-138-210.us-east-2.compute.amazonaws.com:3001',
+      url: process.env.USERS_MICROSERVICE_GRPC,
       package: 'lab',
       protoPath: join(__dirname, '../../../grpc/grpc.proto'),
     },
--- src/infrastructure/config.ts
+++ src/infrastructure/config.ts
@@ -4,4 +4,6 @@ export default async () => ({
   SECRET_KEY: await getParameter('SECRET_KEY'),
   DATABASE_URL: await getParameter('DATABASE_URL'),
   TTL_DELAY: await getParameter('TTL_DELAY'),
+  USERS_MICROSERVICE_GRPC: await getParameter('USERS_MICROSERVICE_GRPC'),
+  GRPC_URL: await getParameter('GRPC_URL'),
 });
